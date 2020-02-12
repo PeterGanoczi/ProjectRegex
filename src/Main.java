2f28ea9ac52ea25bc7bdcf8454e8dc1bfab5ca21
PeterGanoczi <peter.ganoczi@akademiasovy.sk>
2020-02-12 14:41:16 +0100

    regex done

--- src/Main.java
+++ src/Main.java
@@ -6,23 +6,23 @@ import sk.itsovy.data.DataIP;
 import sk.itsovy.data.PersonalID;
 
 public class Main {
+    private static final String IPPATTERN="^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$";
+    private static final String IDPATTERN="^[0-9]{2}[0156][0-9]{3}\\/?[0-9]{3,4}$";
+
     public static void main(String[] args) {
         DataIP data=new DataIP();
         PersonalID personalId=new PersonalID();
 
-
         System.out.println(data.get());
         System.out.println("------Correct IP adresses------");
-        regexIpAdress((ArrayList<String>) data.get());
+        regex((ArrayList<String>) data.get(),IPPATTERN);
 
         System.out.println(personalId.get());
         System.out.println("------Correct personal ID------");
-        regexPersonalId((ArrayList<String>) personalId.get());
+        regex((ArrayList<String>) personalId.get(),IDPATTERN);
     }
 
-    public static void regexIpAdress(ArrayList<String> list){
-
-        String pattern="^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$";
+    public static void regex(ArrayList<String> list, String pattern){
         Pattern pattern1=Pattern.compile(pattern);
 
         for (int i=0; list.size()>i;i++) {
@@ -36,20 +36,4 @@ public class Main {
         }
         System.out.println();
     }
-
-    public static void regexPersonalId(ArrayList<String> list){
-        String pattern="^[0-9]{2}[0156][0-9]{3}\\/?[0-9]{3,4}$";
-        Pattern pattern1=Pattern.compile(pattern);
-
-        for (int i=0; list.size()>i; i++){
-            if (list.get(i)!=null){
-                Matcher matcher=pattern1.matcher(list.get(i));
-                while(matcher.find()){
-                    System.out.println("Found match: "+matcher.group());
-                }
-            }
-        }
-        System.out.println();
-    }
-
 }
